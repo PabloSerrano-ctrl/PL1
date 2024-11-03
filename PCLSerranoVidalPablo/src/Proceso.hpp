3fefc43a9245670ff38e5f9218ea5c01373bd9af
PabloSerrano-ctrl <p.serranov@edu.uah.es>
2024-11-03 13:58:28 +0100

    Update Proceso.hpp

--- PCLSerranoVidalPablo/src/Proceso.hpp
+++ PCLSerranoVidalPablo/src/Proceso.hpp
@@ -1,21 +1,22 @@
 #ifndef PROCESO_HPP
 #define PROCESO_HPP
-
-
+#include <iostream>
 #include <string>
+#include <cstdlib> // Para rand()
+
 class Proceso
 {
 public:
-	Proceso(int PID, std::string usuario, bool tiemporeal);
-	void mostrar() const;
-	~Proceso();
+Proceso(int PID, std::string usuario, bool tiemporeal);
+void mostrar();	
+
 private:
 int PID;
 std::string usuario;
 bool estado; // true: en ejecuci�n, false: parado
 int prioridad;
 bool tiempoReal; // true: tiempo real, false: normal
-void mostrar();
+
 };
 
 #endif // PROCESO_HPP
